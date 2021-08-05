34a7cb58cc76697499ec46cfffa8fc50cd83e949
Sgaggle <88513346+Sgaggle@users.noreply.github.com>
2021-08-05 23:05:02 +0200

    Add files via upload

---
+++ Eifso.py
@@ -0,0 +1,49 @@
+# Learning Python
+
+# 1
+print("Hello World")
+
+#2 indetation number of spaces egal eif mindestens 1. Alles im block gliich
+if 5 > 4:
+    print("feuf isch grösser als 4")
+
+#3 Variable
+x = 5
+y = "Hello World"
+
+#4 Multiline comment
+"""
+Multiline Comment
+"""
+
+#Variablen spezifizieren
+Sttring    =   str(3)   #String
+intigier   =   int(3)   #Int
+floatt     =   float(3) #float
+
+#Get Type
+print(type(Sttring))
+
+#Sonstiges
+"""
+-Variablen sind Case sensitive
+-Strings nutzen single und double quotes
+!Keine nummern am anfang
+!Kein Bindestrich
+!Kein Space
+"""
+
+#Mehrere Variablen auf einmal
+X, Y, Z = 1, 2, 3
+
+#Ein Wert für mehrere Variablen
+X, Y, Z = 1
+
+#Unpack a collection
+fruits = [ "öpfel", "banane","Gurke" ]
+A, B, C = fruits
+
+#Bei Print gleicher Datentyp!
+
+
+
